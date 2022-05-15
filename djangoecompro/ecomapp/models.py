6e06ffd5bec0e510dbce08cbf88de7b462c724c1
Sajan M <sajanmurali@ymail.com>
2022-05-15 16:51:45 +0530

    secondary comits

--- djangoecompro/ecomapp/models.py
+++ djangoecompro/ecomapp/models.py
@@ -1,4 +1,5 @@
 from cgi import print_exception
+from itertools import product
 from unicodedata import name
 from django.db import models
 from django.contrib.auth.models import user
@@ -21,8 +22,29 @@ def __str__(self):
     return self.name
 
 class Order(models.Model):
-    costumer=  models.ForeignKey(Product,on_delete=models.SET_NULL,null=True)
+    costumer=  models.ForeignKey(Costumer,on_delete=models.SET_NULL,null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.IntegerField(default=0,null=True,blank=False)
+    transaction_id= models.CharField(max_length=200,null=True)
+
+
+def __str__(self):
+    return (self.id)
+class OrderItem(models.Model):
+    product=  models.ForeignKey(product,on_delete=models.SET_NULL,null=True)
     order = models.ForeignKey(Order,on_delete=models.SET_NULL,null=True)
-    digital = models.IntegerField(default=0,null=True,blank=False)
-    date_added = models.IntegerField(auto_now_add=True)
-# Create your models here.
+    quantity = models.IntegerField(default=0,null=True,blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class ShippingAddress(models.model):
+    costumer =  models.ForeignKey(Costumer,on_delete=models.SET_NULL,blank=True,null=True)
+    order = models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True,null=True)
+    address=  models.CharField(max_length=200,null=True)
+    city =  models.CharField(max_length=200,null=True)
+    state =  models.CharField(max_length=200,null=True)
+    zipcode =  models.CharField(max_length=200,null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+def __str__(self):
+    return self.address
