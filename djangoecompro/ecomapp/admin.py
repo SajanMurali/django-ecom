bf9456ca1f9337b8b31babae4a6231e9b58f64ac
Sajan M <sajanmurali@ymail.com>
2022-05-16 12:43:25 +0530

    3rd commits

--- djangoecompro/ecomapp/admin.py
+++ djangoecompro/ecomapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Costumer)
+admin.site.register(product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
 
 # Register your models here.
